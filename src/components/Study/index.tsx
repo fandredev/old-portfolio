d42403350f5cdbc2e34f1053bf181491071912ef
profissionalf.andre@gmail.com <profissionalf.andre@gmail.com>
2021-04-19 13:32:36 -0300

    refactor: updated items

--- src/components/Study/index.tsx
+++ src/components/Study/index.tsx
@@ -1,29 +1,21 @@
 import React from "react";
-import { SiAngular, SiNextDotJs } from "react-icons/si";
-import Techs from "../../enums";
+import { SiNextDotJs, SiPython } from "react-icons/si";
+import Techs, { DescTechs } from "../../enums";
 import Card from "../Card";
 import "./styled.scss";
 
 export default function Study(): JSX.Element {
-  const { AngularRxJS, NextJS } = Techs;
+  const { PythonDjango, NextJS } = Techs;
+  const { PythonDjangoDesc, NextJSDesc } = DescTechs;
   return (
     <>
       <h2 id="skills">O que estou estudando?</h2>
       <div id="study">
-        <Card
-          url=""
-          title={AngularRxJS}
-          description="Utilizo diariamente para SPAs,
-          ótima tecnogia com suporte nativo a typescript. RxJS é o conceito de programação reativa muito presente no framework."
-        >
-          <SiAngular size={25} color="#fff" />
-        </Card>
-        <Card
-          url=""
-          title={NextJS}
-          description="Meus estudos com o framework estão iniciando agora. Estou adorando usar ele até o momento, mas, ainda em estudo."
-        >
+        <Card title={NextJS} description={NextJSDesc}>
           <SiNextDotJs size={25} />
+        </Card>
+        <Card title={PythonDjango} description={PythonDjangoDesc}>
+          <SiPython size={25} />
         </Card>
       </div>
     </>
