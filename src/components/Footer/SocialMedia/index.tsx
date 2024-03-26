d6bfdee69014eee61cc35c7f9b6d6aa688e9eed1
felipe <profissionalf.andre@gmail.com>
2024-03-25 23:02:32 -0300

    fix: fixing error eslint control-has-associated-label

--- src/components/Footer/SocialMedia/index.tsx
+++ src/components/Footer/SocialMedia/index.tsx
@@ -1,28 +1,34 @@
 import React from "react";
-import "./styled.scss";
 import { AiOutlineMail } from "react-icons/ai";
 import { FaGithub, FaLinkedin, FaWhatsapp } from "react-icons/fa";
+import "./styled.scss";
 
 export default function SocialMedia(): JSX.Element {
   return (
     <ol>
       <li>
-        <a href="https://github.com/flubyGit">
+        <a href="https://github.com/flubyGit" aria-label="GitHub">
           <FaGithub size={20} />
         </a>
       </li>
       <li>
-        <a href="mailto:profissionalf.andre@gmail.com?subject=Olá Felipe...">
+        <a
+          aria-label="Email"
+          href="mailto:profissionalf.andre@gmail.com?subject=Olá Felipe..."
+        >
           <AiOutlineMail size={20} />
         </a>
       </li>
       <li>
-        <a href="https://www.linkedin.com/in/devfandre/">
+        <a aria-label="Linkedin" href="https://www.linkedin.com/in/devfandre/">
           <FaLinkedin size={20} />
         </a>
       </li>
       <li>
-        <a href="http://wa.me/5511973813705?Olá Felipe...">
+        <a
+          aria-label="Whatsapp"
+          href="http://wa.me/5511973813705?Olá Felipe..."
+        >
           <FaWhatsapp size={20} />
         </a>
       </li>
